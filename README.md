29b464f007ba35c4d54606f937cde35ce356e70b
Viktor Ingemarsson <viktor.ingemarsson@datavarde.se>
2023-08-29 11:44:52 +0200

    first commit slight modded startout project

---
+++ README.md
@@ -0,0 +1,11 @@
+# beam-kotlin-example
+This is a dead simple Kotlin project for getting started with Apache Beam.
+
+More details can be found on the corresponding blog post: https://www.codingblocks.net/programming/getting-started-with-apache-beam-and-kotlin/
+
+
+# How to
+## Build
+mvn package
+## Run
+mvn exec:java -Dexec.mainClass="net.codingblocks.MinimalWordCount"
---
+++ src/main/kotlin/net/codingblocks/Hello.kt
@@ -0,0 +1,27 @@
+package net.codingblocks
+
+import org.apache.beam.sdk.Pipeline
+import org.apache.beam.sdk.io.TextIO
+import org.apache.beam.sdk.transforms.*
+import org.apache.beam.sdk.values.PCollection
+import org.apache.beam.sdk.values.TypeDescriptors
+public object MinimalWordCount {
+        @JvmStatic
+        fun main(args: Array<String>) {
+        val p = Pipeline.create()
+        p
+            .apply<PCollection<String>>(TextIO.read().from("pom.xml"))
+            .apply(
+                    FlatMapElements.into(TypeDescriptors.strings())
+                            .via(ProcessFunction<String, List<String>> { input -> input.toList().map { it.toString() } })
+            )
+            .apply(Count.perElement<String>())
+            .apply(
+                    MapElements.into(TypeDescriptors.strings())
+                            .via(ProcessFunction { input -> "${input.key} : ${input.value}" })
+            )
+            .apply(TextIO.write().to("counts"))
+
+        p.run()
+        }
+}
